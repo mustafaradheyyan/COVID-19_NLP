ad339e7318b0d41cf10794d76448afd41eb37967
Mustafa Radheyyan <mustafa7@pdx.edu>
2021-05-27 03:49:52 -0700

    Finished fixing file names and date formatting

--- modules/process_nlp_output.py
+++ modules/process_nlp_output.py
@@ -1,6 +1,6 @@
 import numpy as np
-import nlp as nlp
-from read_csv import *
+import generate_nlp_output as nlp
+from read_csv_and_get_dict import *
 
 def is_number(price_value):
     try:
@@ -51,30 +51,23 @@ def calculate_keywords(nlp_data):
                 keywords.append(row[position+10:][:-1])
     return keywords
 
-def sort_nlp_output(nlp_file_names, type_of_query):
+def sort_nlp_output(nlp_file_names, nlp_type):
+    file_prefix_len = len('nlp_results_')
     sentiment_dictionary = {}
     keyword_dictionary = {}
     for file in nlp_file_names:
-        date = file[-14:-4]
-        if type_of_query == 'url':
+        date = file[file_prefix_len:-4]
+        if nlp_type[1:] == 'url':
             nlp_data = np.loadtxt(file,dtype=str,delimiter=',',skiprows=1,usecols=(3,7,17))
-        elif type_of_query == 'text':
+        elif nlp_type == 'text':
             nlp_data = np.loadtxt(file,dtype=str,delimiter=',',skiprows=1,usecols=(3,6))
         os.remove(file)
         sentiment_dictionary[date] = calculate_sentiment(nlp_data)
         keyword_dictionary[date] = calculate_keywords(nlp_data)
     return keyword_dictionary, sentiment_dictionary
 
-def get_nlp_keywords_and_sentiment_to_file(nlp_dictionary, type_of_query):
-    nlp_file_names = nlp.generate_nlp_output(nlp_dictionary, type_of_query)
-    keyword_dic, sentiment_dic = sort_nlp_output(nlp_file_names, type_of_query)
-    write_to_file(keyword_dic, 'nlp_keywords.csv', ['Date', 'Keywords'])
-    write_to_file(sentiment_dic, 'nlp_sentiments.csv', ['Date', 'Sentiment'])
-
-def main():
-    pub_csv_header, pub_csv = open_read_csv_file('CSVs/health_publication_information_test.csv')
-    pub_url_dictionary = create_dictionary_object(pub_csv)
-    get_nlp_keywords_and_sentiment_to_file(pub_url_dictionary, 'url')
-
-if __name__ == '__main__':
-  main()
+def get_nlp_keywords_and_sentiment_to_file(nlp_dictionary, nlp_type):
+    nlp_file_names = nlp.generate_nlp_output(nlp_dictionary, nlp_type[-4:])
+    keyword_dic, sentiment_dic = sort_nlp_output(nlp_file_names, nlp_type[-4:])
+    write_to_file(keyword_dic, nlp_type[:-5] + '_nlp_keywords.csv', ['Date', 'Keywords'])
+    write_to_file(sentiment_dic, nlp_type[:-5] + '_nlp_sentiments.csv', ['Date', 'Sentiment'])
