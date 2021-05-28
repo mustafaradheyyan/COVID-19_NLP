2c02700d7158eba92b562e1b81e54b8e3d93409d
Mustafa Radheyyan <mustafa7@pdx.edu>
2021-05-28 07:47:35 -0700

    Changing file directory structure, and adding folders for nlp output

--- modules/run_nlp_health_pub.py
+++
@@ -1,9 +0,0 @@
-from process_nlp_output import *
-
-def main():
-    pub_csv_header, pub_csv = open_read_csv_file('CSVs/health_pubbb.csv')
-    pub_url_dictionary = create_dictionary_object(pub_csv)
-    get_nlp_keywords_and_sentiment_to_file(pub_url_dictionary, 'health_pub..url')
-
-if __name__ == '__main__':
-  main()
