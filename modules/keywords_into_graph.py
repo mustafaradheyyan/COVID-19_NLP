ed36a677547866fa0807361b9dd532174189db3a
Mustafa Radheyyan <mustafa7@pdx.edu>
2021-05-28 06:10:09 -0700

    Added full-screen keyword frequency graph saving without needing to show and maximize picture.

--- modules/keywords_into_graph.py
+++ modules/keywords_into_graph.py
@@ -8,17 +8,6 @@ from read_csv_and_get_dict import *
 from nltk.corpus import stopwords
 from collections import  Counter
 
-#['COVID-19', 'covid-19', 'Covid-19', 'covid19', 'Covid19',
-#                  'COVID19', 'coronavirus', 'Coronavirus', 'SARS-CoV-2', 'new', 'New',
-#                  'use', 'number', 'numbers', 'cases', 'first', 'people', 'Trump',
-#                  'Update', '@realDonaldTrump', 'Record', 'Health', 'health',
-#                  'Summary', 'summary']
-
-#new_stop_words = ['covid19', 'Covid19', 'COVID19', 'coronavirus', 'Coronavirus',
-#                  'SARS-CoV-2', 'new', 'New', 'use', 'number', 'numbers', 'cases',
-#                  'first', 'people', 'Trump', 'Update', '@realDonaldTrump', 'Record',
-#                  'Health', 'health', 'Summary', 'summary']
-
 new_stop_words = ['coronavirus', 'Coronavirus', 'SARS-CoV-2', 'new', 'New', 'use',
                   'number', 'numbers', 'cases', 'first', 'people', 'Trump', 'Update',
                   '@realDonaldTrump', 'Record', 'Health', 'health', 'Summary', 'summary']
@@ -28,8 +17,30 @@ sns.set_context("paper", rc={"font.size":8,"axes.titlesize":25,"axes.labelsize":
 def update_stop_words(keyword):
     new_stop_words.extend(list(map(''.join, itertools.product(*zip(keyword.upper(), keyword.lower())))))
     new_stop_words.extend(list(map(''.join, itertools.product(*zip(keyword.replace('-','').upper(),
-                                                                   keyword.replace('-','').lower())))))
+                                                                keyword.replace('-','').lower())))))
 
+# maximize function code used from https://stackoverflow.com/a/52324347
+def maximize(backend=None,fullscreen=False):
+    """Maximize window independently on backend.
+    Fullscreen sets fullscreen mode, that is same as maximized, but it doesn't have title bar (press key F to toggle full screen mode)."""
+    if backend is None:
+        backend=plt.get_backend()
+    mng = plt.get_current_fig_manager()
+
+    if fullscreen:
+        mng.full_screen_toggle()
+    else:
+        if backend == 'wxAgg':
+            mng.frame.Maximize(True)
+        elif backend == 'Qt4Agg' or backend == 'Qt5Agg':
+            mng.window.showMaximized()
+        elif backend == 'TkAgg':
+            mng.window.state('zoomed') #works fine on Windows!
+        else:
+            print ("Unrecognized backend: ",backend) #not tested on different backends (only Qt)
+
+    plt.pause(0.1) #this is needed to make sure following processing gets applied (e.g. tight_layout)
+    
 def get_top_keywords_per_date_and_frequency(text_dict, keyword):
     x, y = [], []
     stop = set(stopwords.words('english'))
@@ -49,9 +60,8 @@ def customize_keyword_graph(keyword_graph, file_name, keyword):
                              .replace('_', ' ').title() + ' Keyword Frequency over Time')
     keyword_graph.set_xlabel("Keyword\nDate", fontsize = 20)
     keyword_graph.set_ylabel("Frequency")
-    plt.show()
-   # manager = keyword_graph.get_current_fig_manager()
-   # manager.window.showMaximized()
+    #plt.show()
+    maximize(None,False)
     keyword_graph.get_figure().savefig(file_name[:-len('.csv')] + '_graph.png', bbox_inches = 'tight')
 
 def plot_histogram_top_non_stopwords(file_name, text_dict, keyword):
