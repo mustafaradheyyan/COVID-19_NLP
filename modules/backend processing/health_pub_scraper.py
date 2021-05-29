8933df157a971f77190327167bbc9a5cc309e879
Mustafa Radheyyan <mustafa7@pdx.edu>
2021-05-28 21:54:28 -0700

    Added variable search term functionality, removed previous url for multiple keywords.

--- modules/backend processing/health_pub_scraper.py
+++ modules/backend processing/health_pub_scraper.py
@@ -4,17 +4,18 @@ import requests
 from bs4 import BeautifulSoup
 
 def main():
-    URL = 'https://www.medrxiv.org/search/COVID-19%252BOR%252BnCOV%252BOR%252Bnovel%252Bcoronavirus%20jcode%3Amedrxiv%20limit_from%3A2021-05-01%20limit_to%3A2021-05-22%20numresults%3A25%20sort%3Arelevance-rank%20format_result%3Astandard'
+    start_date = '2020-05-01'
+    end_date = '2020-05-22'
+    URL = (f'https://www.medrxiv.org/search/{search_term}%20limit_from%3A{start_date}%20limit_to%'
+           f'3A{end_date}%20numresults%3A25%20sort%3Arelevance-rank%20format_result%3Astandard'
+    #URL = (f'https://www.medrxiv.org/search/COVID-19%252BOR%252BnCOV%252BOR%252B'
+    #      f'novel%252Bcoronavirus%20jcode%3Amedrxiv%20limit_from%3A{start_date}'
+    #      f'%20limit_to%3A{end_date}%20numresults%3A25%20sort%3Arelevance-rank%20'
+    #      f'format_result%3Astandard')
     page = requests.get(URL)
-
     soup = BeautifulSoup(page.content, 'html.parser')
-    #results = soup.find(id = 'zone-content')
-    #print(results.prettify(), file=open('../log.txt', 'w'))
-
-    #results = soup.find_all("href = '/content/10.1101/*')
-    #rows = teamtable.find_all('tr')
+  
     results = soup.find_all('a', href=re.compile('/content/10.1101/*'))
-    #dates = soup.findAll("div", {"id" : lambda L: L and L.startswith('date')})
     print(results, file=open('../log.txt', 'w'))
 
 if __name__ == '__main__':
