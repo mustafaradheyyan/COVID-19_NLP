c600d927bcccd6a5f4c29452bcbb54b999656a75
Mustafa Radheyyan <mustafa7@pdx.edu>
2021-05-29 07:22:01 -0700

    Added calculate_mid_date function to health_pub_scraper.

--- COVID-19_NLP/modules/backend processing/health_pub_scraper.py
+++ COVID-19_NLP/modules/backend processing/health_pub_scraper.py
@@ -8,7 +8,13 @@ from bs4 import BeautifulSoup
 path = 'health_pub'
 if not os.path.exists(path):
     os.mkdir(path)
-    
+
+def calculate_mid_date(start_date, end_date):
+    start_date = datetime.strptime(start_date, "%m-%d-%Y")
+    end_date = datetime.strptime(end_date, "%m-%d-%Y")
+    mid_date = start_date + (end_date - start_date) / 2
+    return mid_date.strftime("%m-%d-%Y")
+
 def health_pub_scraper(number_of_pubs_per_month, start_date, end_date, search_term):
     additional_pages = '?page='
     number_of_results_per_page = 50
@@ -18,21 +24,21 @@ def health_pub_scraper(number_of_pubs_per_month, start_date, end_date, search_te
 
     for month_count in range(0, months + 1):
         # Creating list to append tweet data to
+        url_list = []
         if not months == month_count:
             end_date = calculate_end_date(start_date, end_date)
         else:
             end_date = final_date
         for monthly_page_count in (range(0, (number_of_pubs_per_month / number_of_results_per_page) + 1):
-        
             URL = (f'https://www.medrxiv.org/search/{search_term}%20limit_from%3A{start_date}%20limit_to%'
                    f'3A{end_date}%20numresults%3A{number_of_results_per_page}%20sort%3Arelevance-rank'
                    f'%20format_result%3Astandard{additional_pages}{monthly_page_count}'
             page = requests.get(URL)
             soup = BeautifulSoup(page.content, 'html.parser')
-          
-            url_dict[month_count] = soup.find_all('a', href=re.compile('/content/10.1101/*'))
-            start_date = add_date_by_days(end_date, 1)
+            url_list.append(soup.find_all('a', href=re.compile('/content/10.1101/*')))
 
-    write_to_file(url_dict, search_term + '_url_dict_csv.csv', ['Date', 'URL'])
+        url_dict[calculate_mid_date(start_date, end_date)] = url_list 
+        start_date = add_date_by_days(end_date, 1)
+
+    write_to_file(url_dict, os.path.join(path, search_term + '_url_dict_csv.csv'), ['Date', 'URL'])
     #print(results, file=open('../log.txt', 'w'))
-        
