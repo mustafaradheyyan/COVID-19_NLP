79d56052d3f491fc2e2b9e9b213b8eabfcc675cf
Mustafa Radheyyan <mustafa7@pdx.edu>
2021-05-31 05:01:52 -0700

    Bug fixing completed (keywords and division by zero with sentiment), and added number of keywords per content

--- COVID-19_NLP/modules/backend processing/generate_nlp_output.py
+++ COVID-19_NLP/modules/backend processing/generate_nlp_output.py
@@ -10,29 +10,30 @@ def nlu_authenticator(api_key, service_url):
     natural_language_understanding.set_service_url(service_url)
     return natural_language_understanding
 
-def generate_nlp_output(nlp_input, type_of_query, user_api_key, user_service_url):
+def generate_nlp_output(nlp_input, type_of_query, user_api_key, user_service_url, number_of_keywords):
     nlu = nlu_authenticator(user_api_key, user_service_url)
     if type_of_query[1:] == 'url':
-        return generate_nlp_output_from_url_dict(nlp_input, nlu)
+        return generate_nlp_output_from_url_dict(nlp_input, nlu, number_of_keywords)
     elif type_of_query == 'text':
-        return generate_nlp_output_from_text_dict(nlp_input, nlu)
+        return generate_nlp_output_from_text_dict(nlp_input, nlu, number_of_keywords)
 
-def generate_nlp_output_from_text_dict(text_dict, nlu):
+def generate_nlp_output_from_text_dict(text_dict, nlu, number_of_keywords):
     nlp_dict = {}
     for date, text in text_dict.items():
         nlp_list = []
         for tweet in text:
             nlp_list.append(nlu.analyze(text=tweet,features=Features(keywords=KeywordsOptions\
-            (sentiment=True,emotion=True,limit=2),sentiment=SentimentOptions()),language='en').get_result())
+            (sentiment=True,emotion=True,limit=number_of_keywords),sentiment=SentimentOptions()),\
+            language='en').get_result())
         nlp_dict[date] = nlp_list
     return nlp_dict
 
-def generate_nlp_output_from_url_dict(url_dictionary, nlu):
+def generate_nlp_output_from_url_dict(url_dictionary, nlu, number_of_keywords):
     nlp_dict = {}
     for date, row in url_dictionary.items():
         nlp_list = []
         for url in row:
             nlp_list.append(nlu.analyze(url=url,features=Features(keywords=KeywordsOptions\
-            (sentiment=True,emotion=True,limit=2),sentiment=SentimentOptions())).get_result())
+            (sentiment=True,emotion=True,limit=number_of_keywords),sentiment=SentimentOptions())).get_result())
         nlp_dict[date] = nlp_list
     return nlp_dict
